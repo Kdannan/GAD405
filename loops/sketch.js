57ec6860ce37d37bd89a995211b5a2cf106af4cc
(s) Kira Dannan <kndannan@pm7-0069.uopnet.plymouth.ac.uk>
2018-11-01 10:03:28 +0000

    loops

---
+++ loops/sketch.js
@@ -0,0 +1,24 @@
+//DAT405 / GAD405
+//for P5.js projects
+
+let size = 50;
+
+//initialising function
+function setup() {
+  //create canvas
+  createCanvas(500, 500);
+  //noloop();
+}
+
+function draw() {
+  //set background colour
+  fill (215, 200, 170);
+  for (let x = 0; x < 10; x++){
+    //top row
+    rect(size*x, height/200, size, size);
+    //bottom row
+    rect(size*x, height/2, size, size);
+    //middle row
+    rect(size*x, height/4, size, size);
+  }
+}
