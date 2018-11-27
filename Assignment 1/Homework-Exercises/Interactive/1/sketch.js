7e6942161e2cae327a477b6bcf313dae72d2b8fb
(s) Kira Dannan <kndannan@pm7-0069.uopnet.plymouth.ac.uk>
2018-11-27 14:03:09 +0000

    updates

--- Assignment 1/Homework-Exercises/Interactive/1/sketch.js
+++ Assignment 1/Homework-Exercises/Interactive/1/sketch.js
@@ -1,49 +1,38 @@
-//Create a variable that will store the selection number
-//that defines the shape (via keyboard)
-let selection = 0;
-
 function setup() {
-  createCanvas(1000, 500);
-  strokeWeight(8);
-  noFill();
-  frameRate(10);
-  //noLoop();
+  createCanvas(400, 400); //Use this function to define the size of the output window
 }
 
 function draw() {
-  //background(50);
-  //Create 300 shapes using the for-loop
-  for (var i = 0; i < 300; i++) {
-    push(); //store the matrix before translate
-    //Random position between the size of the canvas
-    translate(random(-100, width+100), random(-100, height+100));
-    //Random rotation
-    rotate(random(2*PI));
-    //Random scale
-    scale(random(3));
+  background(0); //Set your background color to black (0)
 
-    //Create two variables that store the mapped values for mouse position
-    let makeRed = map(mouseX, 0, width, 0, 255);
-    let makeBlue = map(mouseY, 0, height, 0, 255);
+  //Create four variable that will store four different numbers
+  //that are coming from the mouse activity - X, Y, reversed X, reversed Y
+  //To achieve this we use the map function that takes the values from the
+  //XY motion of the mouse on the window size, and creates another number
+  //from 0 to 255 to control the color of each of the four rectangles
+  var mouse1 = map(mouseX, 0, width, 0,   255);
+  var mouse2 = map(mouseY, 0, width, 0,   255);
+  var mouse3 = map(mouseX, 0, width, 255, 0);
+  var mouse4 = map(mouseY, 0, width, 255, 0);
 
-    //Mouse interaction defines the color of the strokes
-    stroke(makeRed, random(255), makeBlue);
-    //The fill color is black, but with a transparency variation
-    fill(0, random(255));
+  //Four different values are created. The alpha channel is controlled
+  //by the mouse variables we created before
+  var color1 = color(255, 0,   0,   mouse1);
+  var color2 = color(0,   255, 0,   mouse2);
+  var color3 = color(255, 255, 0,   mouse3);
+  var color4 = color(0,   255, 255, mouse4);
 
-    //Selection of colors
-    //Based on the key pressed (defined outside the for-loop),
-    //the selection variable will either be 0, 1, or 2.
-    //This selected one of the colors defined below
-    if (selection===0) ellipse(0, 0, 40, 40);
-    if (selection===1) rect(0, 0, 40, 40);
-    if (selection===2) triangle(30, 75, 58, 20, 86, 75);
+  //Each color variable is set to a different fill function
+  //that colors each rectangle
+  fill(color1);
+  rect(0, 0, width/2, height/2);
 
-    pop(); //reset the matrix to original state
-  }
+  fill(color2);
+  rect(width/2, 0, width/2, height/2);
 
-  //According to the key pressed, change the number of the selection variable
-  if (key === 'a') selection = 0;
-  if (key === 's') selection = 1;
-  if (key === 'd') selection = 2;
+  fill(color3);
+  rect(0, height/2, width/2, height/2);
+
+  fill(color4);
+  rect(width/2, height/2, width/2, height/2);
 }
